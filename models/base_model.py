0a75dcc104015563bb3770fa49d75205493d67cd
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 09:42:45 +0000

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,43 @@
-#!/usr/bin/python3
-from uuid import uuid4
+#!/usr/bin/Python3
+import uuid
 from datetime import datetime
-""" a model that defines all common attributes/methods for other classes """
+"""BaseModel that defines all common attributes/methods for other classes:"""
 
 
 class BaseModel:
-    """ simple class with name BaseModel for this project"""
-    def __init__(self, *arg, **kwargs):
-        """ Initialize instance"""
-        self.id = str(uuid4())
+    """Simple Base Model class"""
+    def __init__(self, *args, **kwargs):
+        """initialize imprtant instance attribute"""
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        """dictionary representation of an instance (method to_dict())."""
+
         '''if len(kwargs) != 0:
-            strtime = "%Y- %m-%dT%H:%M:%S.%f"
+            strft = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, strtime)
-                else:
-                    self.__dict__[key] = value
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        self.__dict__[key] = datetime.strptime(value, strft)
+                    else:
+                        self.__dict__[key] = value
         else:
-            models.storage.new(self)'''
+            models.storage.new(self) '''
 
     def save(self):
-        """ updates datetime of updated time to current time """
+        """Update the current datetime"""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values"""
-        kdict = self.__dict__.copy()
-        kdict["__class__"] = self.__class__.__name__
-        kdict["created_at"] = self.created_at.isoformat()
-        kdict["updated_at"] = self.updated_at.isoformat()
-        return kdict
+        """Returns the dict format of an object"""
+        kvdict = self.__dict__.copy()
+        # strtf = "%Y-%m-%dT%H:%M:%S.%f"
+        kvdict["__class__"] = type(self).__name__
+        kvdict["updated_at"] = self.updated_at.isoformat()
+        kvdict["created_at"] = self.created_at.isoformat()
+        return kvdict
 
     def __str__(self):
-        """ prints [<class name>] (<self.id>) <self.__dict__> """
-        return f"[{type(self).__name__}] ({self.id}) {seld.__dict__}"
+        """Returns information about the class in human readable format"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
