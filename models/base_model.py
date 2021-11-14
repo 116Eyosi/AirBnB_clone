90e9451562a26733f4ecd0bfb3b9e75405ced550
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 09:24:52 +0000

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import models
 from uuid import uuid4
 from datetime import datetime
 """ a model that defines all common attributes/methods for other classes """
@@ -12,7 +11,7 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        '''if len(kwargs) != 0:
             strtime = "%Y- %m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -20,7 +19,7 @@ class BaseModel:
                 else:
                     self.__dict__[key] = value
         else:
-            models.storage.new(self)
+            models.storage.new(self)'''
 
     def save(self):
         """ updates datetime of updated time to current time """
