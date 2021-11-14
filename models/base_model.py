c9e885eebc5d73c5e5344f614360a6873d53b7db
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 09:30:37 +0000

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 class BaseModel:
     """ simple class with name BaseModel for this project"""
-    '''def __init__(self, *arg, **kwargs):
+    def __init__(self, *arg, **kwargs):
         """ Initialize instance"""
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-         if len(kwargs) != 0:
+        '''if len(kwargs) != 0:
             strtime = "%Y- %m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
