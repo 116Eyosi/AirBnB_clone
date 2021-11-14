1b292f1faeebd7868959cced313a8f2f6355eb9c
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 09:29:28 +0000

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from uuid import uuid4
-from datetome import datetime
+from datetime import datetime
 """ a model that defines all common attributes/methods for other classes """
 
 
@@ -11,7 +11,7 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+         if len(kwargs) != 0:
             strtime = "%Y- %m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
