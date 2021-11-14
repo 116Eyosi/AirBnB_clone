2df9a2c4fa6515f18adec3d603f0f32cb8b33fc5
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 09:44:54 +0000

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel:
     """Simple Base Model class"""
     def __init__(self, *args, **kwargs):
         """initialize imprtant instance attribute"""
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         """dictionary representation of an instance (method to_dict())."""
