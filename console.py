d47561471c35d3258a688d33870626d0799a07a3
Eyosias Nigussie <116eyosi@gmail.com>
2021-11-14 15:15:40 +0000

    test push

---
+++ console.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Command Interpreter """
+
+from models.base_model import BaseModel
+from models import storage
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    ''' Entry point of console program and commands center '''
+
+    intro = ''
+    prompt = '(hbnb) '
+
+    def do_create(self, arg):
+        """ creates new object """
+
+        if self.validate_class(arg):
+            base_model = BaseModel()
+            base_model.save()
+            print(base_model.id)
+
+    def do_show(self, arg):
+        """ shows object of given id """
+
+        args = arg.split()
+        if self.validate_class(args[0]):
+            if not ".".join(args) in storage.all().keys():
+                print("Not Found")
+            else:
+                print(storage.all()[".".join(args)])
+
+    def emptyline(arg):
+        """ when no command given """
+
+        pass
+
+    def do_EOF(self, arg):
+        '''Quit command to exit the program
+        '''
+
+        print("")
+        return True
+
+    def do_quit(self, arg):
+        '''Quit command to exit the program
+        '''
+
+        return True
+
+    def validate_class(self, arg):
+        """ validates the given class name """
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return False
+        elif not arg == str(type(BaseModel()).__name__):
+            print("** class doesn't exist **")
+            return False
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,73 @@
 #!/usr/bin/python3
-import models
-import uuid
-from datetime import datetime
-""" a model that defines all common attributes/methods for other classes """
+"""
+Custom base class for the entire project
+"""
 
+from uuid import uuid4
+from datetime import datetime
+import models
 
 class BaseModel:
-    """ simple class with name BaseModel for this project"""
-    def __init__(self, *arg, **kwargs):
-        """ Initialize instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        '''if len(kwargs) != 0:
-            strtime = "%Y- %m-%dT%H:%M:%S.%f"
+    """Custom base for all the classes in the AirBnb console project
+    Arttributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
+    Methods:
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance arttributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Public instance artributes initialization
+        after creation
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+        """
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+        else:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, strtime)
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
                 else:
                     self.__dict__[key] = value
-        else:
-            models.storage.new(self)'''
-
-    def save(self):
-        """ updates datetime of updated time to current time """
-        self.updated_at = datetime.today()
-        # models.storage.save()
-
-    def to_dict(self):
-        """ returns a dictionary containing all keys/values"""
-        kdict = self.__dict__.copy()
-        kdict["__class__"] = type(self).__name__
-        kdict["created_at"] = self.created_at.isoformat()
-        kdict["updated_at"] = self.updated_at.isoformat()
-        return kdict
 
     def __str__(self):
-        """ prints [<class name>] (<self.id>) <self.__dict__> """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """
+        Returns string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Method returns a dictionary containing all 
+        keys/values of __dict__ instance
+        """
+        map_objects = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                map_objects[key] = value.isoformat()
+            else:
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
